60a4b6467d468959cc415c166f3a7e703a9fc936
Steve Hirschhorn <hirschhorn970@gmail.com>
2015-02-04 16:02:07 -0700

    made it through CRUD

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -32,6 +32,12 @@ class BlogPostsController < ApplicationController
     end
   end
 
+  def destroy
+    blog_post = BlogPost.find(params[:id])
+    blog_post.destroy
+    redirect_to blog_posts_path
+  end
+
   private
 
   def blog_post_params
